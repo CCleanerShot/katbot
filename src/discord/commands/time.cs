7bb88820d959c971b2bf59292fc78b67f612162d
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-02-17 22:44:53 +0000

    month fix

--- src/discord/commands/time.cs
+++ src/discord/commands/time.cs
@@ -7,7 +7,7 @@ public partial class DiscordCommands : InteractionModuleBase
     [SlashCommand("time", "its time to get a watch")]
     public async Task time()
     {
-        string time = DateTime.UtcNow.ToString("dddd, dd MM yyyy HH:mm");
+        string time = DateTime.UtcNow.ToString("dddd, dd MMMM yyyy HH:mm");
 
         await RespondAsync($"It is `{time}`.");
     }
