63652403037fa67106055f9790d28e0d8f219bad
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-02-17 22:47:10 +0000

    easter_egg under time

--- src/discord/commands/time.cs
+++ src/discord/commands/time.cs
@@ -7,8 +7,17 @@ public partial class DiscordCommands : InteractionModuleBase
     [SlashCommand("time", "its time to get a watch")]
     public async Task time()
     {
-        string time = DateTime.UtcNow.ToString("dddd, dd MMMM yyyy HH:mm");
+        int chance = Program.Utility.NextRange(1, 100);
 
-        await RespondAsync($"It is `{time}`.");
+        if (chance == 1)
+        {
+            await RespondAsync("It's time for you to get a watch");
+        }
+
+        else
+        {
+            string time = DateTime.UtcNow.ToString("dddd, dd MMMM yyyy HH:mm");
+            await RespondAsync($"It is `{time}`.");
+        }
     }
 }
