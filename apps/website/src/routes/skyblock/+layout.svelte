ac418770b6ddb47b7b407590f5658ee494695a73
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-04-11 09:07:15 +0000

    testing new route

--- apps/website/src/routes/skyblock/+layout.svelte
+++ apps/website/src/routes/skyblock/+layout.svelte
@@ -12,7 +12,7 @@
 	const { children } = $props();
 
 	onMount(() => {
-		const url = `ws://${window.location.href}:${PUBLIC_PORT_WEBSOCKET}`;
+		const url = `ws://${window.location.host}:${PUBLIC_PORT_WEBSOCKET}`;
 		socketState.socketService = new WebsocketService(url);
 	});
 </script>
